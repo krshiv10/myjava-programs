0b44b5159f0ab1626157fa095b04f538ae13abe1
shivam kumar <45265208+krshiv10@users.noreply.github.com>
2019-02-12 12:08:28 +0530

    First file uploded
    Java program of maximum of three numbers

---
+++ maximum.java
@@ -0,0 +1,21 @@
+class maximum 
+{
+public static void main(String args[])
+{
+int a=10,b=20,c=30;
+if (a>b && a>c )
+{
+ System.out.println("a is greater");
+}
+else if( b>a && b>c) 
+{
+system.out.println("max no. is b");
+}
+else 
+{
+system.out.println("max no.is c");
+  }
+
+
+}
+   
