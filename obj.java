366f1626db5a0fb2bd665cf4713e2830c2f75228
shivam kumar <45265208+krshiv10@users.noreply.github.com>
2019-02-12 12:13:32 +0530

    Update obj.java

--- obj.java
+++ obj.java
@@ -1,15 +1,20 @@
-class Classobject
+class B
+{
+ public void fun()
+ {
+  System.out.printf("hello world");
+  }
+}
+
+
+
+
+
+class A
 {
   public static void main(String args[])
    {
      abc obj=new abc();
      obj.fun();
    }
-}
-class abc
-{
- public void fun()
- {
-  System.out.printf("hello world");
-  }
 }
