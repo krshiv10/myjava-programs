4b2a29d2dc8ae2124b1fd8a7cf21155e3a07bc58
shivam kumar <45265208+krshiv10@users.noreply.github.com>
2019-02-12 12:10:42 +0530

    program using classes and object in java

---
+++ obj.java
@@ -0,0 +1,15 @@
+class Classobject
+{
+  public static void main(String args[])
+   {
+     abc obj=new abc();
+     obj.fun();
+   }
+}
+class abc
+{
+ public void fun()
+ {
+  System.out.printf("hello world");
+  }
+}
